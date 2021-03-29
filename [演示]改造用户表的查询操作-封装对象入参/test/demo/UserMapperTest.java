00d9d5d8770672eb3011fa8e591db1d60fdfbe2d
yanchao00551 <10947@163.com>
2021-03-29 11:45:50 +0800

    案例提交

---
+++ [演示]改造用户表的查询操作-封装对象入参/test/demo/UserMapperTest.java
@@ -0,0 +1,116 @@
+package demo;
+
+import demo.dao.user.UserMapper;
+import demo.dao.util.MyBatisUtil;
+import demo.entity.SkeyeduUser;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @PackageName:demo
+ * @ClassName:UserMapperTest
+ * @Description:
+ * @author: 悟空
+ * @date: 2021/3/25 10:54
+ * @email: 10947@163.com
+ */
+
+@Slf4j
+public class UserMapperTest {
+
+    @Test
+    public void test(){
+        SqlSession sqlSession = null;
+        List<SkeyeduUser> users = new ArrayList<>();
+        try{
+            sqlSession = MyBatisUtil.createSqlSession();
+            //第一种方式：调用selectList方法执行查询操作
+            //users = sqlSession.selectList("abc.getUserList");
+
+            //第二种方式：调用getMapper(Mapper.class)执行dao接口方法来实现对数据库的查询操作
+            users = sqlSession.getMapper(UserMapper.class).getUserList();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            MyBatisUtil.closeSqlSession(sqlSession);
+        }
+
+        //java 8新特性
+        users.forEach((value) -> log.info("每行的内容：{}",value));
+    }
+
+    @Test
+    public void testGetUserListByUserName(){
+        SqlSession sqlSession = null;
+        List<SkeyeduUser> users = new ArrayList<>();
+        try{
+            sqlSession = MyBatisUtil.createSqlSession();
+            //第二种方式：调用getMapper(Mapper.class)执行dao接口方法来实现对数据库的查询操作
+            users = sqlSession.getMapper(UserMapper.class).getUserListByUserName("陈");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            MyBatisUtil.closeSqlSession(sqlSession);
+        }
+
+        //java 8新特性
+        users.forEach((value) -> log.info("每行的内容：{}",value));
+    }
+
+
+    @Test
+    public void testGetUserList(){
+        SqlSession sqlSession = null;
+        List<SkeyeduUser> users = new ArrayList<>();
+        try{
+            sqlSession = MyBatisUtil.createSqlSession();
+            //第二种方式：调用getMapper(Mapper.class)执行dao接口方法来实现对数据库的查询操作
+            SkeyeduUser skeyeduUser = new SkeyeduUser();
+            skeyeduUser.setUserName("光");
+            skeyeduUser.setType(1);
+            //javabean传参
+            users = sqlSession.getMapper(UserMapper.class).queryUserList(skeyeduUser);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            MyBatisUtil.closeSqlSession(sqlSession);
+        }
+
+        //java 8新特性
+        users.forEach((value) -> log.info("每行的内容：{}",value));
+    }
+
+
+    @Test
+    public void testGetUserListByMap(){
+        SqlSession sqlSession = null;
+        List<SkeyeduUser> users = new ArrayList<>();
+        try{
+            sqlSession = MyBatisUtil.createSqlSession();
+            //第二种方式：调用getMapper(Mapper.class)执行dao接口方法来实现对数据库的查询操作
+            Map<String,String> userMap = new HashMap<String,String>();
+            userMap.put("lName","g");
+            userMap.put("sex","1");
+            //javabean传参
+            users = sqlSession.getMapper(UserMapper.class).getUserListByMap(userMap);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            MyBatisUtil.closeSqlSession(sqlSession);
+        }
+
+        //java 8新特性
+        users.forEach((value) -> log.info("每行的内容：{}",value));
+    }
+}
---
+++ [演示]根据用户名对用户表进行模糊查询/src/demo/dao/user/UserMapper.java
@@ -0,0 +1,46 @@
+package demo.dao.user;
+
+import demo.entity.SkeyeduUser;
+
+import java.util.List;
+
+/**
+ * @Author 悟空
+ * @Description //TODO 
+ * @Date 11:34 2021/3/26
+ * @Param 
+ * @return 
+ **/
+
+public interface UserMapper {
+
+    /**
+     * 查询用户记录数.
+     * @Author 悟空
+     * @Description //TODO
+     * @Date 11:36 2021/3/26
+     * @Param []
+     * @return int
+     **/
+    int count();
+
+    /**
+     * 查询用户列表.
+     * @Author 悟空
+     * @Description //TODO
+     * @Date 10:14 2021/3/26
+     * @Param []
+     * @return java.util.List<demo.entity.SkeyeduUser>
+     **/
+    List<SkeyeduUser> getUserList();
+
+    /**
+     * 根据用户名称查询用户列表（模糊查询）
+     * @Author 悟空
+     * @Description //TODO
+     * @Date 9:52 2021/3/29
+     * @Param [userName]
+     * @return java.util.List<demo.entity.SkeyeduUser>
+     **/
+    List<SkeyeduUser> getUserListByUserName(String userName);
+}
