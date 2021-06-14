4bb6a74a36c5d91798bed144b8497b1b3ab51543
yanchao00551 <10947@163.com>
2021-06-14 18:35:10 +0800

    tj

---
+++ [演示]mybatis逆向工程/src/com/jiudian/modules/sys/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.jiudian.modules.sys.mapper;
+
+import com.jiudian.modules.sys.entity.User;
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    List<User> selectAll();
+
+    int updateByPrimaryKey(User record);
+}
